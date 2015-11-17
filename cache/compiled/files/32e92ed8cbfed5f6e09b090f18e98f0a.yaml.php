9b2ed8b1351da892f079bde5d20b8b8aa79fb37d
Vitor Costa <vmcosta@me.com>
2015-11-17 11:33:37 +0000

    Initial commit

--- cache/compiled/files/32e92ed8cbfed5f6e09b090f18e98f0a.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'user/config/plugins/highlight.yaml',
-    'modified' => 1447344729,
-    'data' => [
-        'theme' => 'monokai_sublime'
-    ]
-];
